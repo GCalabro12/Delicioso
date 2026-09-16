ef598902c36f3684784ef77b3a2eaf9c182bc967
GCalabro12 <118734009+GCalabro12@users.noreply.github.com>
2026-09-16 15:07:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'delicoso-v12';
+var ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      var fetchPromise = fetch(e.request).then(function(networkResponse){
+        caches.open(CACHE_NAME).then(function(cache){ cache.put(e.request, networkResponse.clone()); });
+        return networkResponse;
+      }).catch(function(){ return cached; });
+      return cached || fetchPromise;
+    })
+  );
+});
